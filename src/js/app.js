ea1c5ebb0679dffc0ee94cd8b081ca7c8d233916
ivaylo91 <ipenev91@gmail.com>
2022-12-15 18:53:19 +0200

    scale avatar profile

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const element = document.querySelector(".image");
+  element.addEventListener("click", function() {
+    element.style.transform = "scale(2)";
+  });
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
